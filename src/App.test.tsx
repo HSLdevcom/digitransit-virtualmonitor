3943da078fa072391cd391cdb8e299429887715e
Eero Tanskanen <eero.tanskanen@haaveinc.com>
2018-07-04 12:12:20 +0300

    Testing changes. Added react-test-renderer.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import { StaticRouter } from 'react-router';
+import { create } from 'react-test-renderer';
 
-import App from './App';
+import App from 'src/App';
+import StopIncomingRetriever from './ui/StopIncomingRetriever';
 
-const WrappedApp = () => (
-  <StaticRouter>
+const WrappedApp = ({ location = '' }: { location?: string} ) => (
+  <StaticRouter location={location}>
     <App />
   </StaticRouter>
 );
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WrappedApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const renderer = create(<WrappedApp />);
+  renderer.unmount();
+});
+
+it('renders a StopIncomingRetriever when there\'s "/stop/623" in url', () => {
+  const renderer = create(<WrappedApp location={'/stop/623'} />);
+
+  expect(renderer.root.findByType(StopIncomingRetriever));
+  // console.log (renderer.root.findAllByType(StopIncomingRetriever, { deep: true }));
+  // expect(renderer.root.findAllByType(StopIncomingRetriever, { deep: true }));
 });

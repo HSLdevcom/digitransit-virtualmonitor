f56a7541f030720c4915e34c25ccc314571d9df9
Eero Tanskanen <eero.tanskanen@haaveinc.com>
2018-08-20 16:04:27 +0300

    Added copy-to-clipboard package. Can copy configurations to clipboard.

--- src/ui/ConfigEditor.tsx
+++ src/ui/ConfigEditor.tsx
@@ -1,7 +1,10 @@
+import * as copy from "copy-to-clipboard";
 import * as React from "react";
 
 import { IConfiguration } from "./ConfigurationList";
 import DisplayEditor from "./DisplayEditor";
+
+const copyConfigurationToClipboard = (configuration: IConfiguration) => () => copy(JSON.stringify(configuration));
 
 const ConfigEditor = ({ configuration }: { configuration: IConfiguration }) => (
   <div>
@@ -15,7 +18,7 @@ const ConfigEditor = ({ configuration }: { configuration: IConfiguration }) => (
     ))}
     <label>JSON: </label>
     <textarea value={JSON.stringify(configuration)} />
-    {/* <button onClick={() => { navigator.clipboard.writeText(JSON.stringify(configuration)) }} value={'Copy JSON to clipboard'} /> */}
+    <button onClick={copyConfigurationToClipboard(configuration)} value={'Copy JSON to clipboard'}>Copy JSON to clipboard</button>
   </div>
 );
 

262dd642e22d4d508b086973259767f5c909f5b8
Eero Tanskanen <eero.tanskanen@haaveinc.com>
2018-09-07 17:27:14 +0300

    Schema Display separated into Display & View.

--- src/graphQL/SDisplay.ts
+++ src/graphQL/SDisplay.ts
@@ -1,16 +1,24 @@
-import { GraphQLObjectType, GraphQLNonNull, GraphQLList } from "graphql";
+import { GraphQLObjectType, GraphQLNonNull, GraphQLList, GraphQLFloat } from "graphql";
 
 import SPosition from "./SPosition";
 import SStop from "src/graphQL/SStop";
 import STranslatedString from "src/graphQL/STranslatedString";
+import SView from 'src/graphQL/SView';
+
+const SViewWithDisplayTime = new GraphQLObjectType({
+  fields: {
+    view: { type: SView },
+    displayTime: { type: GraphQLFloat },
+  },
+  name: 'ViewWithDisplayTime',
+});
 
 const SDisplay = new GraphQLObjectType({
   fields: {
-    position: { type: SPosition },
-    stops: {
-      resolve: (root, {}) => Object.values(root.stops),
-      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(SStop)))
+    viewCarousel: {
+      type: new GraphQLNonNull(new GraphQLList(SViewWithDisplayTime)),
     },
+    position: { type: SPosition },
     title: { type: new GraphQLNonNull(STranslatedString) },
   },
   name: 'Display',
--- src/graphQL/SView.ts
+++ src/graphQL/SView.ts
@@ -1,12 +1,10 @@
 import { GraphQLObjectType, GraphQLNonNull, GraphQLList } from "graphql";
 
-import SPosition from "src/graphQL/SPosition";
 import SStop from "src/graphQL/SStop";
 import STranslatedString from "src/graphQL/STranslatedString";
 
 const SView = new GraphQLObjectType({
   fields: {
-    position: { type: SPosition },
     stops: {
       resolve: (root, {}) => Object.values(root.stops),
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(SStop)))

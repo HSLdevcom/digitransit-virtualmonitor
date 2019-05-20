17d47d79e00b9966c008290ca4c5bd4aca3a0768
antikainenjo <janne.o.antikainen@cgi.com>
2019-05-20 15:00:59 +0300

    Docker image build script and travis conf

---
+++ build-docker-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ORG=${ORG:-hsldevcom}
+DOCKER_IMAGE=digitransit-virtualmonitor
+
+DOCKER_TAG="ci-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}"
+# Set these environment variables
+#DOCKER_USER=
+#DOCKER_AUTH=
+
+function tagandpush {
+  docker tag $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:$1
+  docker push $ORG/$DOCKER_IMAGE:$1
+}
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  docker login -u $DOCKER_USER -p $DOCKER_AUTH
+  echo "processing master build $TRAVIS_COMMIT"
+  #master branch, build and tag as latest
+  docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
+  docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
+  tagandpush $TRAVIS_BRANCH
+  fi
+else
+  echo "processing pr $TRAVIS_PULL_REQUEST"
+  docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
+fi

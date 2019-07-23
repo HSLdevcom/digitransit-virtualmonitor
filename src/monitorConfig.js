25440d62ba41d49b164761ec00b19a254caacba9
Antiik91 <antiik91@gmail.com>
2019-07-23 14:21:04 +0300

    Matka configuration to use MATKA data and updated help page accordingly

--- src/monitorConfig.js
+++ src/monitorConfig.js
@@ -18,11 +18,11 @@ export default {
     },
     matka: {
       feedId: 'matka',
-      uri: 'https://api.digitransit.fi/routing/v1/routers/waltti/index/graphql', 
-      urlParamUsageText: 'Pysäkkinäytön käyttö selaimen osoiteriviltä tapahtuu seuraavasti: kirjoita osoitteen perään /stop/pysäkit/rivimäärä. Esimerkiksi /stop/Kuopio:201646/10 Näyttää 10 riviä pysäkiltä Puijonlaaksontie E',
-      urlMultipleStopsText: 'Usean pysäkin näytön saat yksinkertaisesti lisäämällä pysäkkejä pilkulla erotettuna, esimerkiksi /stop/Kuopio:201646,Kuopio:201854/10.',
+      uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql', 
+      urlParamUsageText: 'Pysäkkinäytön käyttö selaimen osoiteriviltä tapahtuu seuraavasti: kirjoita osoitteen perään /stop/pysäkit/rivimäärä. Esimerkiksi /stop/MATKA:7_201860/10 Näyttää 10 riviä pysäkiltä Umpikuja E',
+      urlMultipleStopsText: 'Usean pysäkin näytön saat yksinkertaisesti lisäämällä pysäkkejä pilkulla erotettuna, esimerkiksi /stop/MATKA:7_201860,MATKA:7_201848/10.',
       urlParamFindText: 'Löydät oikean pysäkki-id:n helposti esimerkiksi reittioppaasta: Siirry halutun Waltti-kaupungin pysäkin pysäkkinäkymään, ja katso urlin lopusta pysäkki-id. ',
-      urlParamFindAltText: 'Esimerkiksi kuopio.digitransit.fi/pysakit/Kuopio%3A201646 nähdään, että pysäkki-id on Kuopio:201646. Huomaathan että %3A on HTML koodia, ja se tarkoittaa : merkkiä.',
+      urlParamFindAltText: 'Esimerkiksi https://opas.matka.fi/pysakit/MATKA%3A7_201848 nähdään, että pysäkki-id on MATKA:7_201848. Huomaathan että %3A on HTML koodia, ja se tarkoittaa : merkkiä.',
     },
 };
 

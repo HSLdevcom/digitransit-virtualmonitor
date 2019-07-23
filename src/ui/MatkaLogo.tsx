51b4d1d50b1def869253ae59d11e9b1f864484af
Antiik91 <antiik91@gmail.com>
2019-07-23 12:25:30 +0300

    Matka.fi logo to svg

--- src/ui/MatkaLogo.tsx
+++ src/ui/MatkaLogo.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
-import * as Logo from 'src/ui/matka-logo.png'
+import * as Logo from 'src/ui/matka-logo.svg'
 
 export default ({ style }: {style?: React.CSSProperties} = { style: {} }) => (
-  <img height="80%" src={Logo}  alt="" />
+  <svg
+  id={"logo"}
+  viewBox={"0 0 160 60"}
+  version={"1.1"}
+  preserveAspectRatio={"xMidYMid meet"}
+  style={{ color: 'gray', ...style }}
+>
+ <image href={Logo} width={"100%"} height={"100%"} />
+</svg>
 );

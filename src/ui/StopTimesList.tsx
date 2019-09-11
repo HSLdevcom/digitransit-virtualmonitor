d740fbd4396ed9b218f9dda39ad982b6c96d2d2d
Antiik91 <antiik91@gmail.com>
2019-09-11 10:36:23 +0300

    Comment tweaking

--- src/ui/StopTimesList.tsx
+++ src/ui/StopTimesList.tsx
@@ -47,7 +47,7 @@ const StopTimesListHeadersTranslated = translate('translations')(StopTimesListHe
 const StopTimeRow = ({ stoptime, showPier, t, showStopColumn } : { stoptime: IStopTime & IOverrideStopName, showPier?: boolean, showStopColumn?: boolean, } & InjectedTranslateProps) => {
   const isCanceled = stoptime.realtimeState === 'CANCELED';
 
-  // If the Vehicle is arriving to its' destination, its headsign is null, or stop's name.
+  // If the Vehicle is arriving to its' destination, its headsign is null, or headsign is stop's name.
   // Check that the vehicle is arriving, and that the stop is indeed last stop of the route. 
   let isLastStopTerminal = false;
   const isArrival = stoptime.pickupType === 'NONE';

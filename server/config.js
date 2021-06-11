6b196409d10c3c42927d13dd377a350cf78f14cd
MikkoPuustinen <mikkowr@gmail.com>
2021-06-11 15:17:34 +0300

    chore: env variables

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 const config = {
-    endpoint: process.env.DB_ENDPOINT || 'X',
-    key: process.env.DB_KEY || 'X',
-    databaseId: process.env.DB_ID || 'X',
-    containerId: process.env.DB_CONTAINER || 'X',
+    endpoint: process.env.APPSETTING_DB_ENDPOINT || 'X',
+    key: process.env.APPSETTING_DB_KEY || 'X',
+    databaseId: process.env.APPSETTING_DB_ID || 'X',
+    containerId: process.env.APPSETTING_DB_CONTAINER || 'X',
     partitionKey: { kind:  'Hash', paths: ['/contenthash'] }
 };
 module.exports = config;

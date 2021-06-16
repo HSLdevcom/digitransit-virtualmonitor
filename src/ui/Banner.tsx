b846da5a38941142810b75bd02b4c16244d22936
Janne Antikainen <antiik91@gmail.com>
2021-06-16 08:30:03 +0300

    lint

--- src/ui/Banner.tsx
+++ src/ui/Banner.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './Banner.scss';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import Icon from "./Icon";
+import Icon from './Icon';
 import Logo from './logo/Logo';
 import { slide as Menu } from 'react-burger-menu';
 interface Props {
@@ -17,7 +17,11 @@ const Banner: React.FC<Props & WithTranslation> = (
   return (
     <div className="banner">
       <Logo monitorConfig={props.config} />
-      <Menu right width="400px" customCrossIcon={<Icon img="close"  color="#1c57cf" width={25} />}>
+      <Menu
+        right
+        width="400px"
+        customCrossIcon={<Icon img="close" color="#1c57cf" width={25} />}
+      >
         <div className="lang-section">
           <span
             className="lang-select"
@@ -42,9 +46,15 @@ const Banner: React.FC<Props & WithTranslation> = (
           </span>
         </div>
         <div className="link-section">
-          <Link className="link" to={'/'}>{'Etusivu'}</Link>
-          <Link className="link" to={'/createView'}>{props.t('quickDisplayCreate')}</Link>
-          <Link className="link" to={'/help'}>{'Apua'}</Link>
+          <Link className="link" to={'/'}>
+            {'Etusivu'}
+          </Link>
+          <Link className="link" to={'/createView'}>
+            {props.t('quickDisplayCreate')}
+          </Link>
+          <Link className="link" to={'/help'}>
+            {'Apua'}
+          </Link>
         </div>
       </Menu>
     </div>

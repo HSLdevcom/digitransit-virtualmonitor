7c9bc40bdf81a951024de9fe4fff5d715b9cea81
Janne Antikainen <antiik91@gmail.com>
2021-06-22 08:19:48 +0300

    fix typo and other little things

--- src/ui/Breadcrumbs.tsx
+++ src/ui/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import './Breadcrubms.scss';
+import './Breadcrumbs.scss';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import i18n from '../i18n';

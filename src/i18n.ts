677542648e4f8038c5878d74b490c12ab2b5b197
Kimmo Hujanen <kimmo.hujanen@cgi.com>
2021-09-10 12:25:42 +0300

    added fi: renameDestination

--- src/i18n.ts
+++ src/i18n.ts
@@ -203,6 +203,7 @@ i18n.init({
         prepareStop: 'Lisää pysäkki',
         previewView: 'Esikatsele',
         quickDisplayCreate: 'Luo uusi näyttö',
+        renameDestinations: 'Muokkaa määränpää-tekstejä',
         renamedDestinations: 'Muutetut määränpäät: ',
         save: 'Tallenna',
         search: 'Etsi',

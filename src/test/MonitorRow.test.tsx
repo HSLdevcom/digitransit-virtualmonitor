443b42cb96c0ec0ad9c8855ebaa7ab117e66abfe
MikkoPuustinen <mikkowr@gmail.com>
2022-08-30 15:43:47 +0300

    chore: fix tests

--- src/test/MonitorRow.test.tsx
+++ src/test/MonitorRow.test.tsx
@@ -4,6 +4,7 @@ import { Router, MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import MonitorRow from '../ui/MonitorRow';
 import { departure, stop } from './data/monitor';
+import { ConfigContext } from '../contexts';
 
 const defaultProps = {
   alertState: 0,
@@ -20,8 +21,20 @@ const defaultProps = {
   withoutRouteColumn: false,
 };
 
+const mockConfig = {
+  useTilde: true,
+};
+
+const withContext = prps => {
+  return (
+    <ConfigContext.Provider value={mockConfig}>
+      <MonitorRow {...prps} />
+    </ConfigContext.Provider>
+  );
+};
+
 it('should render a row correctly', () => {
-  const { container } = render(<MonitorRow {...defaultProps} />);
+  const { container } = render(withContext(defaultProps));
   // destination
   expect(
     container.getElementsByClassName('destination-row')[0].innerHTML,
@@ -48,7 +61,7 @@ it('should render a cancelled departure with the text cancelled', () => {
     alertState: 1,
   };
 
-  const { container } = render(<MonitorRow {...props} />);
+  const { container } = render(withContext(props));
   expect(
     container.getElementsByClassName('cancelled-row')[0].innerHTML,
   ).toEqual('cancelled');
@@ -65,7 +78,7 @@ it('should render a cancelled departure with alert icon and destination', () => 
     departure: dep,
   };
 
-  const { container } = render(<MonitorRow {...props} />);
+  const { container } = render(withContext(props));
 
   expect(
     container.getElementsByClassName('destination-row')[0].innerHTML,
@@ -87,7 +100,7 @@ it('should do show tilde when realtime is off', () => {
     departure: dep,
   };
 
-  const { container } = render(<MonitorRow {...props} />);
+  const { container } = render(withContext(props));
   expect(
     container.getElementsByClassName('grid-col time')[0].innerHTML,
   ).toContain('~');

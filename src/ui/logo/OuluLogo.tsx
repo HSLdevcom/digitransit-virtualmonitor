848bacf1f4e013985dc253b3149cd6e87535ea9a
Harhio <sade.harhio@cgi.com>
2023-02-16 11:11:52 +0200

    DT-5713 oulu svg logo

--- src/ui/logo/OuluLogo.tsx
+++ src/ui/logo/OuluLogo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Logo from './OSL_Logo_Valkoinen_RGB.png';
+import Logo from './osl-logo.svg';
 
 export default ({ style }: { style?: React.CSSProperties } = { style: {} }) => (
   <div className="oulu">
-    <img title="logo oulu" src={Logo} style={style} />
+    <img title="logo oulu" className="oulu" src={Logo} style={style} />
   </div>
 );
